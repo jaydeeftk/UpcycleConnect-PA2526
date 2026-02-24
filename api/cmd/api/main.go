76ed36e8791b08c4df9d453b1e363856ecb2db54
Jaydee <jaydee.phanoukoun@gmail.com>
2026-02-24 03:02:42 +0100

    api : add main server entrypoint

---
+++ api/cmd/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "log"
+  "net/http"
+
+  "upcycleconnect/internal/routes"
+
+  )
+
+func main () {
+  mux := routes.NewRouter()
+
+  addr := ":8080"
+  log.Printf("API listening on %s", addr)
+
+  if err := http.ListenAndServe(addr, mux); err != nil {
+    log.Fatal(err)
+    }
+}
